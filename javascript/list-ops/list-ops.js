60523fdeaa7cd172bf0ecef90fd01a40c7d598be
chandrakishor singh <chandrakishors52@gmail.com>
2021-07-15 16:52:28 +0530

    Add list-ops solution

--- javascript/list-ops/list-ops.js
+++ javascript/list-ops/list-ops.js
@@ -1,42 +1,72 @@
 //
-// This is only a SKELETON file for the 'List Ops' exercise. It's been provided as a
-// convenience to get you started writing code faster.
+// You can find my solutions to other JavaScript challenges in my git repository.
+// https://github.com/chandrakishorSingh/exercism-solutions/tree/master/javascript
 //
 
 export class List {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  constructor(arr) {
+    this.values = [];
+
+    if (arr) {
+      for (let item of arr) {
+        if (typeof(item) === 'object' && item.constructor === List) {
+          this.values = [...this.values, ...item.values];
+        } else {
+          this.values = [...this.values, item];
+        }
+      }
+    }
   }
 
-  append() {
-    throw new Error('Remove this statement and implement this function');
+  append(list) {
+    return new List([...this.values, ...list.values]);
   }
 
-  concat() {
-    throw new Error('Remove this statement and implement this function');
+  concat(list) {
+    return new List([...this.values, ...list.values]);
   }
 
-  filter() {
-    throw new Error('Remove this statement and implement this function');
+  filter(func) {
+    let values = [];
+    for (let ele of this.values) {
+      if (func(ele)) {
+      values = [...values, ele];
+      }
+    }
+    return new List(values);
   }
 
-  map() {
-    throw new Error('Remove this statement and implement this function');
+  map(func) {
+    let values = [];
+    for (let ele of this.values) {
+      values = [...values, func(ele)];
+    }
+    return new List(values);
   }
 
   length() {
-    throw new Error('Remove this statement and implement this function');
+    return this.values.length;
   }
 
-  foldl() {
-    throw new Error('Remove this statement and implement this function');
+  foldl(func, acc) {
+    for (let ele of this.values) {
+      acc = func(acc, ele);
+    }
+    return acc;
   }
 
-  foldr() {
-    throw new Error('Remove this statement and implement this function');
+  foldr(func, acc) {
+    for (let i = this.values.length - 1; i > -1; i--) {
+      acc = func(acc, this.values[i]);
+    }
+    return acc;
   }
 
   reverse() {
-    throw new Error('Remove this statement and implement this function');
+    let values = [];
+    for (let i = this.values.length - 1; i > -1; i--) {
+      values = [...values, this.values[i]];
+    }
+    return new List(values);
   }
 }
